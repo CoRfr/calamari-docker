b734ae6b2154ca39ce6f4dde14f0b4705308c533
Bertrand Roussel <broussel@sierrawireless.com>
2015-02-26 11:16:16 -0800

    Adding sample .service files

--- minion/entrypoint.sh
+++ minion/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOG_LEVEL=${LOG_LEVEL:-debug}
+
 if [ -z "$MASTER_IP" ]; then
     echo "MASTER_IP not provided"
     exit 1
@@ -7,5 +9,12 @@ fi
 
 echo "master: $MASTER_IP" > /etc/salt/minion.d/calamari.conf
 
-salt-minion -l info
+salt-minion -l ${LOG_LEVEL} &
+SALT_PID=$!
+
+while ! ls /etc/diamond/diamond.conf > /dev/null 2&>1; do
+    sleep 1
+done
+
+diamond -f -l
 
